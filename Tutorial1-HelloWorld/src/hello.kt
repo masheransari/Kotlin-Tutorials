5b296f70a28c3ab8717b09c78bd74b20460163dc
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2018-06-14 11:47:55 +0500

    Tutorial 1 added.

---
+++ Tutorial1-HelloWorld/src/hello.kt
@@ -0,0 +1,25 @@
+//esme humien class ki need nhe hoti...
+
+
+fun main(args: Array<String>) {
+    print("Message");
+    mainVoid();
+    println("\n"+mainInt());
+    print(10f/9);
+}
+////this is actually in java.
+//public static void main(String[] args){
+//
+//}
+
+
+//for return type method
+
+//void show Unit in kotlin
+fun mainVoid(): Unit {
+}
+
+fun mainInt(): Int {
+
+    return 2;
+}
