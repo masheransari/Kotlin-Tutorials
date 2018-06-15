82a527f916604d7c0521908f1bf445100b74be75
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2018-06-15 17:28:40 +0500

    Tutorial 7 ForLoop Added

---
+++ Tutorial7-Loops/src/myFirst.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    //Simple For Loop
+    //for (initialization  range){
+        //  Body Of Loop
+    // }
+
+
+
+
+//    for (i in 0..10) {
+//        println("Counter: " + i);
+//    }
+
+
+
+    //Even Odd Condition.
+    for (i in 0..10) {
+        if (i % 2 == 0) {
+            println("Even: " + i);
+        }else{
+            println("Odd: "+i);
+        }
+    }
+}
